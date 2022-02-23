9f46851b489c5c7070c66e615f9ef404462a5c06
chengyichaos <1120535228@qq.com>
2022-02-23 17:35:12 +0800

    client class finished

--- client.cpp
+++ client.cpp
@@ -1,47 +1,67 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/shm.h>
-#include <iostream>
-using namespace std;
-int main()
-{
-    //定义sockfd,功能：创建一个新的套接字，返回套接字描述符
-    int sock_cli = socket(AF_INET,SOCK_STREAM, 0);
+#include "client.h"
+
+client::client(int port,string ip):server_port(port),server_ip(ip){}
+client::~client(){
+    close(sock);
+}
+void client::run(){
+
+    //定义sockfd,客户端套接子
+    sock = socket(AF_INET,SOCK_STREAM, 0);
 
     //定义sockaddr_in
     struct sockaddr_in servaddr;
     memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;//TCP/IP协议族
-    servaddr.sin_port = htons(8023);  //服务器端口
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");  //服务器ip
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(server_port);  //服务器端口
+    servaddr.sin_addr.s_addr = inet_addr(server_ip.c_str());  //服务器ip
 
     //连接服务器，成功返回0，错误返回-1
-    if (connect(sock_cli, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    if (connect(sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
     {
         perror("connect");
         exit(1);
     }
-    cout<<"连接服务器成功！\n";
+    cout<<"连接服务器成功\n";
 
+    //创建发送线程和接收线程
+    thread send_t(SendMsg,sock),recv_t(RecvMsg,sock);
+    send_t.join();
+    cout<<"发送线程已结束\n";
+    recv_t.join();
+    cout<<"接收线程已结束\n";
+    return;
+}
+
+
+//注意，前面不用加static！
+void client::SendMsg(int conn){
     char sendbuf[100];
-    char recvbuf[100];
     while (1)
     {
         memset(sendbuf, 0, sizeof(sendbuf));
         cin>>sendbuf;
-	cout<<"sizeof"<<sizeof(sendbuf)<<endl;
-	cout<<"strlen"<<strlen(sendbuf)<<endl;
-        send(sock_cli, sendbuf, strlen(sendbuf),0); //发送
-        if(strcmp(sendbuf,"exit")==0)
+        int ret=send(conn, sendbuf, strlen(sendbuf),0); //发送
+        //输入exit或者对端关闭时结束
+        if(strcmp(sendbuf,"exit")==0||ret<=0)
             break;
     }
-    close(sock_cli);
-    return 0;
+}
+
+
+
+//注意，前面不用加static！
+void client::RecvMsg(int conn){
+    //接收缓冲区
+    char buffer[1000];
+    //不断接收数据
+    while(1)
+    {
+        memset(buffer,0,sizeof(buffer));
+        int len = recv(conn, buffer, sizeof(buffer),0);
+        //recv返回值小于等于0，退出
+        if(len<=0)
+            break;
+        cout<<"收到服务器发来的信息："<<buffer<<endl;
+    }
 }
